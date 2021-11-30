1764e255c95a57e6776bf9ee5b61b11ce51d9547
Dave Wallace <dave.wallace@totaralearning.com>
2021-11-30 13:06:19 +1300

    exercise 1 complete

--- src/01/index.js
+++ src/01/index.js
@@ -1,5 +1,6 @@
 const elBox = document.querySelector('#box');
 
+/*
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
@@ -8,18 +9,53 @@ function transition(state, event) {
     // Add your state/event transitions here
     // to determine and return the next state
   ) {
+    case 'inactive':
+      switch (event) {
+        case 'CLICK':
+          return 'active';
+          default: state;
+      }
+    case 'active':
+      switch (event) {
+        case 'CLICK':
+          return 'inactive';
+        default: state;
+      }
+    default:
+      return state;
   }
 }
+*/
+
+const machine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        'CLICK': 'active'
+      }
+    },
+    active: {
+      on: {
+        'CLICK': 'inactive'
+      }
+    }
+  }
+};
+
+function transition(state, event) {
+  return machine.states[state]?.on[event] || state;
+};
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = machine.initial;
 
 function send(event) {
   // Determine the next value of `currentState`
-
+  currentState = transition(currentState, event);
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
-  // send a click event
+  send('CLICK');
 });
